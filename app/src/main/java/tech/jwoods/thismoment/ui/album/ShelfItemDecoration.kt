2cf70925782768eb01c2c3b485fea9b555d715ac
Jessica <jessica@jwoods.tech>
2021-05-01 23:01:37 +1000

    Refactor ShelfItemDecoration

--- app/src/main/java/tech/jwoods/thismoment/ui/album/ShelfItemDecoration.kt
+++ app/src/main/java/tech/jwoods/thismoment/ui/album/ShelfItemDecoration.kt
@@ -5,19 +5,19 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
-import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 
-// Solution Adapted from here: https://stackoverflow.com/questions/24618829/how-to-add-dividers-and-spaces-between-items-in-recyclerview?page=1&tab=votes#tab-top
+// Solution Adapted from: https://stackoverflow.com/questions/24618829/how-to-add-dividers-and-spaces-between-items-in-recyclerview?page=1&tab=votes#tab-top
 
 class ShelfItemDecoration : ItemDecoration {
     private var divider: Drawable?
 
     // Default divider
     constructor(context: Context) {
-        val styledAttributes: TypedArray = context.obtainStyledAttributes(ATTRS)
+        val styledAttributes: TypedArray = context.obtainStyledAttributes(intArrayOf(R.attr.listDivider))
         divider = styledAttributes.getDrawable(0)
         styledAttributes.recycle()
     }
@@ -27,21 +27,17 @@ class ShelfItemDecoration : ItemDecoration {
         divider = ContextCompat.getDrawable(context, resId)
     }
 
-    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        val left = parent.paddingLeft
-        val right = parent.width - parent.paddingRight
-        val childCount = parent.childCount
-        for (i in 0 until childCount) {
-            val child: View = parent.getChildAt(i)
-            val params = child.layoutParams as RecyclerView.LayoutParams
-            val top: Int = child.bottom - (divider!!.intrinsicHeight / 2)
-            val bottom = top + divider!!.intrinsicHeight
-            divider!!.setBounds(left, top, right, bottom)
-            divider!!.draw(c)
-        }
-    }
+    override fun onDraw(canvas: Canvas, parent: RecyclerView) {
+        divider?.let { divider ->
+            val left = parent.paddingLeft
+            val right = parent.width - parent.paddingRight
 
-    companion object {
-        private val ATTRS = intArrayOf(R.attr.listDivider)
+            for (child in parent.children) {
+                val top = child.bottom - (divider.intrinsicHeight / 2)
+                val bottom = top + divider.intrinsicHeight
+                divider.setBounds(left, top, right, bottom)
+                divider.draw(canvas)
+            }
+        }
     }
 }
